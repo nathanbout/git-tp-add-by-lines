75cbe62eb74830737f595d4721f80490b988a29c
Nathan Bouthors <nathan.bouthors@etu.minesparis.psl.eu>
2024-09-22 13:17:32 +0200

    ajout de la version précédente en vue de la v3

--- fibonacci.py
+++ fibonacci.py
@@ -10,3 +10,16 @@ def fibo_cached(n):
         return n
     else:
         return fibo_cached(n-1) + fibo_cached(n-2)
+
+"""
+la suite de fibonacci
+"""
+
+def fibo(n):
+    """
+    # une implémentation naïve et inefficace
+    """
+    if n <= 1:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
